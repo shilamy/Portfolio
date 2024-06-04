10d81ec446fb2b83a148ee4d6c96d89599cb21ba
shilamy <macmynev@gmail.com>
2024-06-04 11:29:37 +0300

    First commit on the site

---
+++ main.js
@@ -0,0 +1,10 @@
+var typed= new Typed(".text",{
+    strings: ["Frontend Development","Graphics Designs","Software Development"],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+})
+
+
+
